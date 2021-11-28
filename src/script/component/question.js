e8d6abd7741d165cb3a3e0a4f2559935fdc93d5a
snifee <razyaarf@gmail.com>
2021-11-28 14:32:42 +0800

    mengubah element html pada question.js

--- src/script/component/question.js
+++ src/script/component/question.js
@@ -17,26 +17,26 @@ class Question extends HTMLElement{
     render(){
         this._shadowRoot.innerHTML = `
             <div class="mb-3">
-                <label for="question${this.questionNumber}" class="form-label">${this.question}</label>
-                <div class="Answer">
+                <label for="question-${this.questionNumber}" class="form-label">${this.question}</label>
+                <div class="answer-${this.questionNumber}">
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1">
+                        <input class="form-check-input" type="radio" name="${this.questionNumber}" id="inlineRadio1" value="1">
                         <label class="form-check-label" for="inlineRadio1">1</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2">
+                        <input class="form-check-input" type="radio" name="${this.questionNumber}" id="inlineRadio2" value="2">
                         <label class="form-check-label" for="inlineRadio2">2</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="3">
+                        <input class="form-check-input" type="radio" name="${this.questionNumber}" id="inlineRadio3" value="3">
                         <label class="form-check-label" for="inlineRadio3">3</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="4">
+                        <input class="form-check-input" type="radio" name="${this.questionNumber}" id="inlineRadio3" value="4">
                         <label class="form-check-label" for="inlineRadio3">4</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="5">
+                        <input class="form-check-input" type="radio" name="${this.questionNumber}" id="inlineRadio3" value="5">
                         <label class="form-check-label" for="inlineRadio3">5</label>
                     </div>
                 </div>
